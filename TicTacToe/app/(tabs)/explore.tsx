0aef33a7f33007a572b372078b7430dca898c3dd
DiazLuisAlejandro <alejandrodiazluisv@gmail.com>
2025-10-30 19:37:43 +0000

    Clase 30 octubre

--- TicTacToe/app/(tabs)/explore.tsx
+++ TicTacToe/app/(tabs)/explore.tsx
@@ -8,15 +8,16 @@ import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Fonts } from '@/constants/theme';
+import React from 'react';
 
 export default function TabTwoScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
+      headerBackgroundColor={{ light: '#ffffffff', dark: '#000000ff' }}
       headerImage={
         <IconSymbol
           size={310}
-          color="#808080"
+          color="#808080" 
           name="chevron.left.forwardslash.chevron.right"
           style={styles.headerImage}
         />
